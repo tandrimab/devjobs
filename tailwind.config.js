9f9a442da6efbe5ad790ee10c69e86b4727280e9
tandrimab <bhattacharjee.tan@outlook.com>
2024-02-01 12:47:23 +0530

    Added tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,45 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    fontFamily: {
+      'bold': ['Kumbh sans_bold'],
+      'extraBold': ['Kumbh sans_extraBold'],
+      'medium': ['Kumbh sans_medium'],
+      'thin': ['Kumbh sans_thin'],
+      'semiBold': ['Kumbh sans_semiBold']
+    },
+    fontSize: {
+      base: ['1rem', {
+        lineHeight: '1.625rem',
+        fontWeight: 400,
+      }],
+      sm: ['0.875rem', {
+        lineHeight: '1.125rem',
+        fontWeight: 300,
+      }],
+      '3xl': ['1.75rem', {
+        lineHeight: '2.125rem',
+        fontWeight: 600,
+      }],
+      '2xl': ['1.5rem', {
+        lineHeight: '1.8125rem',
+        fontWeight: 500,
+      }],
+      xl: ['1.25rem', {
+        lineHeight: '1.5rem',
+        fontWeight: 400,
+      }],
+    },
+    
+    opacity: {
+      1: '0.1',
+      10: '0.1035',
+      25: '0.25',
+      35: '0.35',
+      5: '0.5'
+    }
   },
   plugins: [],
 }
