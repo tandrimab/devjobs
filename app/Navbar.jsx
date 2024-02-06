f1372cf4731d938dcd81f4cb07a11144ce5adf19
tandrimab <bhattacharjee.tan@outlook.com>
2024-02-06 10:49:50 +0530

    Implementation of navbar

--- app/Navbar.jsx
+++ app/Navbar.jsx
@@ -5,9 +5,9 @@ import Link from "next/link";
 
 
 export default function Navbar() {
-    return (<nav className="bg-lightBlue pt-8 pb-14 rounded-bl-100 bg-no-repeat nav-bg">
-        <div className='flex item-center justify-between max-w-[80%] mx-auto px-24 '>
-            <Link href="/">
+    return (<nav className="bg-lightBlue lg:rounded-bl-100 md:rounded-bl-100 sm:rounded-none bg-no-repeat nav-bg md:pt-3 md:pb-4 lg:pt-4 lg:pb-8">
+        <div className='flex item-center justify-between lg:max-w-[85%] max-w-full mx-auto lg:px-24 md:px-8 sm:px-8 sm:py-6 '>
+            <Link href="/" className="sm:max-w-[8rem]">
                 <Image src="/assets/logos/devjobs.svg" width={200} height={200} className='py-4' alt="logo" />
             </Link>
             <div className="flex item-center h-max ml-auto my-auto z-[999]">
