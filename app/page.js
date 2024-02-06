ce85b2c7f3506dd7584972e5c77083676baac161
tandrimab <bhattacharjee.tan@outlook.com>
2024-02-06 10:52:25 +0530

    Implementation of homepage

--- app/page.js
+++
@@ -1,7 +0,0 @@
-
-export default async function Home() {
-    return (
-      <main className=" min-h-screen flex items-center justify-between flex-col max-w-[85%] p-24 mx-auto">
-      </main>
-    )
-  }
