2f7241f88e4d0ca80935efe7aa34e88ed3ae2646
tandrimab <bhattacharjee.tan@outlook.com>
2024-02-08 10:43:33 +0530

    Added NextAuth provider for Github and Google

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth";
+import GithubProviders from 'next-auth/providers/github';
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+    providers: [
+        GithubProviders({
+            clientId: process.env.GITHUB_ID ?? '',
+            clientSecret: process.env.GITHUB_SECRET ?? ''
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET
+        })
+    ]
+}
+
+export const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
