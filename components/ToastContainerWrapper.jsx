75401dcb37d9fae1c2de94095ef2af77399fd97a
tandrimab <bhattacharjee.tan@outlook.com>
2024-05-09 13:39:34 +0530

    Implementation of toast container wrapper

---
+++ components/ToastContainerWrapper.jsx
@@ -0,0 +1,19 @@
+import { ToastContainer } from "react-toastify";
+
+
+export default function ToastContainerWrapper() {
+    return (
+        <ToastContainer
+        position="top-right"
+        autoClose={3500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    )
+}
