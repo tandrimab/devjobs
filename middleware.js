d1400b893c89c85d6d40c97e13d57a9277b80566
tandrimab <bhattacharjee.tan@outlook.com>
2024-06-05 18:46:54 +0530

    Implementation: Added middleware for authorization

---
+++ middleware.js
@@ -0,0 +1,57 @@
+import ApiError from "./utilities/backend/ApiError";
+import { NextResponse } from "next/server";
+import { decode } from "./utilities/backend/jwt";
+
+export async function middleware(request) {
+  const cookie = request.cookies.get("next-auth.session-token");
+
+  try {
+    if (!cookie || !cookie.value) {
+      throw new ApiError(401, "Authentication required");
+    }
+
+    if (request.nextUrl.pathname.endsWith("/user") && request.method === 'POST') {
+      const { token } = await request.json();
+
+      const decoded = await decode({token});
+
+      const decodedToken = decoded.payload;
+
+      if (
+        !decodedToken ||
+        !decodedToken.email ||
+        !decodedToken.access_token ||
+        !decodedToken.refresh_token
+      ) {
+        if (!decodedToken) {
+          throw new ApiError(400, "Missing request body");
+        }
+
+        if (!decodedToken.email) {
+          throw new ApiError(400, "Missing email in the request body");
+        }
+
+        if (!decodedToken.access_token || !decodedToken.refresh_token) {
+          throw new ApiError(401, decodedToken.error || "Authentication required.");
+        }
+      }
+    }
+    
+    return NextResponse.next();
+
+  } catch (e) {
+    return NextResponse.json(
+      {
+        message: e.message,
+        ...e,
+      },
+      {
+        status: e.code || 500,
+      }
+    );
+  }
+}
+
+export const config = {
+  matcher: '/api/user',
+}
