120aafb4ed71098806e25cdff88d124f408db4e3
tandrimab <bhattacharjee.tan@outlook.com>
2024-08-12 12:07:58 +0530

    Added transform-scale property for onhover effect on buttons and job card

--- components/JobCardBase.jsx
+++ components/JobCardBase.jsx
@@ -4,19 +4,19 @@ import Link from "next/link";
 
 export default function JobCardBase(props) {
     return (
-        <div className="bg-white rounded-[6px] px-8 py-8 cursor-pointer lg:max-w-md max-w-full relative mt-8 sm:mx-auto sm:w-full">
-            <Link href={`/jobs/${encodeURIComponent(props.data.id)}`} key={props.id}>
-                <div className="h-[50px] w-[50px] rounded-[12px] absolute top-[-1.5rem] flex" style={{ backgroundColor: props.data.companyDetails.logoBackground }}>
-                    <Image className="items-center justify-center m-auto" src={props.data.companyDetails.logo} height={20} width={20} alt="logos" />
+        <div className="bg-white rounded-[6px] px-8 py-8 cursor-pointer lg:max-w-md max-w-full relative mt-8 sm:mx-auto sm:w-full hover:transition-all hover:delay-100 hover:duration-500 hover:scale-110">
+            <Link href={`/jobs/${encodeURIComponent(props.data?.id)}`} key={props.id}>
+                <div className="h-[50px] w-[50px] rounded-[12px] absolute top-[-1.5rem] flex" style={{ backgroundColor: props.data?.companyDetails?.logoBackground }}>
+                    <Image className="items-center justify-center m-auto" src={props.data?.companyDetails?.logo} height={20} width={20} alt="logos" />
                 </div>
                 <div className="flex text-darkGrey font-base mt-4">
-                    <p className="">{props.data.postedAt}</p>
+                    <p className="">{props.data?.postedAt}</p>
                     <div className="rounded-full bg-darkGrey h-[4px] w-[4px] mx-4 my-auto" />
-                    <p>{props.data.contract}</p>
+                    <p>{props.data?.contract}</p>
                 </div>
-                <p className="text-xl hover:text-darkGrey font-bold mt-4">{props.data.position}</p>
-                <p className="text-base text-darkGrey mt-4">{props.data.companyDetails.name}</p>
-                <p className="text-sm text-lightBlue font-bold mt-6">{props.data.location}</p>
+                <p className="text-xl hover:text-darkGrey font-bold mt-4">{props.data?.position}</p>
+                <p className="text-base text-darkGrey mt-4">{props.data?.companyDetails?.name}</p>
+                <p className="text-sm text-lightBlue font-bold mt-6">{props.data?.location}</p>
             </Link>
         </div>
     )
--- components/LoginButtons.jsx
+++ components/LoginButtons.jsx
@@ -1,56 +1,69 @@
-'use client'
-import Icon from '@mdi/react';
-import { mdiGithub, mdiGoogle, mdiMicrosoftOutlook } from '@mdi/js';
+"use client";
+import Icon from "@mdi/react";
+import { mdiGithub, mdiGoogle, mdiMicrosoftOutlook } from "@mdi/js";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import { usePathStore } from '@/app/store/store';
-import AnimatedLogo from './AnimatedLogo';
+import { usePathStore } from "@/app/store/store";
+import AnimatedLogo from "./AnimatedLogo";
+import Image from "next/image";
 
 export default function LoginButtons() {
-    const { data: session } = useSession();
-    const router  = useRouter();
-    const { prevPath } = usePathStore()    
+  const { data: session } = useSession();
+  const router = useRouter();
+  const { prevPath } = usePathStore();
 
-    useEffect(() => {
-        if (session?.user) {
-            router.push(prevPath);
-        }
-    }, [session, router])
+  useEffect(() => {
+    if (session?.user) {
+      router.push(prevPath);
+    }
+  }, [session, router]);
 
-
-    return (
-        <div className="max-w-[80%] rounded-[12px] md:ml-auto sm:mx-auto animate-scaleRight bg-white shadow-lg"
-        // style={{backgroundImage: 'linear-gradient(315deg, rgba(43,60,60,1) 0%, rgba(45,103,120,1) 40%, rgba(45,107,126,1) 58%, rgba(33,47,74,1) 100%, rgba(7,2,17,1) 100%)'}}
-        >
-            <AnimatedLogo />
-            <div className="flex flex-col items-center">
-                
-                <div className="w-10/12 flex items-center flex-col py-10 px-10">
-                    <div className="rounded-[6px] md:px-4 flex flex-row max-w-8/12 mx-auto border-[2px] border-[#9e7f66] justify-center items-center cursor-pointer shadow hover:shadow-xl my-5">
-                        <Icon path={mdiGithub} size='2.5rem' />
-                        <button onClick={() => signIn('github')} className="text-base text-darkBlue font-medium py-2 px-8 shrink-0">
-                            Signin with Github
-                        </button>
-                    </div>
-                    <div className="rounded-[6px] md:px-4 flex flex-row max-w-8/12 mx-auto border-[2px] border-[#9e7f66] justify-center items-center cursor-pointer my-5 shadow hover:shadow-xl">
-                        <Icon path={mdiGoogle} size='2.5rem' />
-                        <button onClick={() => 
-                            signIn('google')} className="text-base text-darkBlue font-medium py-2 px-8 shrink-0">
-                            Signin with Google
-                        </button>
-                    </div>
-                    <div className="rounded-[6px] md:px-4 flex flex-row max-w-8/12 mx-auto border-[2px] border-[#9e7f66] justify-center items-center cursor-pointer my-5 shadow hover:shadow-xl">
-                        <Icon path={mdiMicrosoftOutlook} size='2.5rem' />
-                        <button onClick={() => 
-                            signIn(
-                                'azure-ad',
-                                { callbackUrl: '/' })} className="text-base font-medium py-2 px-8 shrink-0 text-darkBlue">
-                            Signin with Outlook
-                        </button>
-                    </div>
-                </div>
+  return (
+    <div className="h-fit ">
+      <div className="flex flex-col items-center justify-center">
+        <div className="w-10/12 flex items-center flex-col py-10 px-10">
+          <div className="rounded-[6px] md:px-4 flex flex-row max-w-8/12 mx-auto border-[1px] border-lightBlue justify-center items-center cursor-pointer hover:transition-all hover:delay-100 hover:duration-500 hover:scale-110 my-5">
+            <Icon path={mdiGithub} size="2.5rem" color="white" />
+            <button
+              onClick={() => signIn("github")}
+              className="text-base text-darkBlue font-medium py-2 px-8 shrink-0"
+            >
+              Signin with Github
+            </button>
+          </div>
+          <div className="rounded-[6px] md:px-4 flex flex-row max-w-8/12 mx-auto border-[1px] border-lightBlue justify-center items-center cursor-pointer my-5 hover:transition-all hover:delay-100 hover:duration-500 hover:scale-110">
+            <Icon path={mdiGoogle} size="2.5rem" color="white" />
+            <button
+              onClick={() => signIn("google")}
+              className="text-base text-darkBlue font-medium py-2 px-8 shrink-0"
+            >
+              Signin with Google
+            </button>
+          </div>
+          <div className="rounded-[6px] md:px-4 flex flex-row max-w-8/12 mx-auto border-[1px] border-lightBlue justify-center items-center cursor-pointer my-5 hover:transition-all hover:delay-100 hover:duration-500 hover:scale-110">
+            <Icon path={mdiMicrosoftOutlook} size="2.5rem" color="white" />
+            <button
+              onClick={() => signIn("azure-ad", { callbackUrl: "/" })}
+              className="text-base font-medium py-2 px-8 shrink-0 text-darkBlue"
+            >
+              Signin with Outlook
+            </button>
+          </div>
+          <a href="/">
+            <div className="flex gap-3 mt-6 text-sm">
+              <Image
+                src="/assets/desktop/arrow.svg"
+                height={20}
+                width={40}
+                alt="arrow"
+                className="rotate-[180deg]"
+              />
+              <p className="text-darkBlue">Back to Home</p>
             </div>
+          </a>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
