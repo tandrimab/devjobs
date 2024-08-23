ad031fde217e190aeee7a6ad65e6daaa37ec2400
tandrimab <bhattacharjee.tan@outlook.com>
2024-08-23 17:14:12 +0530

    Fix: Added CORS headers to avoid CORS issue

--- next.config.js
+++ next.config.js
@@ -1,8 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
   env: {
-    NEXTAUTH_URL: "https://devjobs-delta.vercel.app/",
+    NEXTAUTH_URL: "https://devjobs-delta.vercel.app",
     NEXTAUTH_SECRET: "svodEw3MakE1yixdmWLM1G1LZIUHSZ+J9NGvT4WzzPk=",
 
     GITHUB_ID: "Iv23li5UwsqYdjmoqWmh",
