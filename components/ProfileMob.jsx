679fa8bee3b2486eaf6685bdee1a1f534451afc6
tandrimab <bhattacharjee.tan@outlook.com>
2024-08-24 01:24:41 +0530

    Modify: Revamped the sidebar profile

--- components/ProfileMob.jsx
+++ components/ProfileMob.jsx
@@ -1,53 +1,154 @@
 import Switch from "@/components/Switch";
-import { mdiMenu } from "@mdi/js";
 import Icon from "@mdi/react";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import {
+  mdiBriefcaseOutline,
+  mdiAccountEdit,
+  mdiLockOffOutline,
+  mdiAccountDetailsOutline,
+  mdiMenu,
+} from "@mdi/js";
 
 export default function ProfileMob({ session, login, signout }) {
-    const [open, setOpen] = useState(false);
+    const profileRef = useRef(null);
+  const [open, setOpen] = useState(false);
 
-    console.log(open);
-    
+  useEffect(() => {
+    const handleClick = (e) => {
+        if (!profileRef.current.contains(e.target)) {
+            setOpen(false);
+        }
+    }
 
-    return (
-        <div className="group relative flex items-center ml-6 ">
-            <Icon
-                path={mdiMenu}
-                size="2rem"
-                className="text-white cursor-pointer"
-                onClick={() => setOpen(!open)}
+    document.addEventListener('mousedown', handleClick);
+
+    return () => {
+        document.removeEventListener('mousedown', handleClick);
+    }
+  }, [])
+
+  return (
+    <div className="group relative flex items-center ml-6" ref={profileRef}>
+      <Icon
+        path={mdiMenu}
+        size="2rem"
+        className="text-white cursor-pointer"
+        onClick={() => setOpen(!open)}
+      />
+
+      <div
+        className={
+          "absolute top-[50px] right-[5px] group-hover:block bg-btnDarkHover rounded-[12px] z-[999] text-white min-w-[300px] " +
+          (open ? "block" : "hidden")
+        }
+      >
+        {session ? (
+          <div className="px-6 py-4 flex items-center border-b-veryLightBlue border-b ">
+            <Image
+              height={30}
+              width={30}
+              src={session?.image}
+              className="rounded-full max-w-[30px] max-h-[30px] mr-4"
+              referrerPolicy="no-referrer"
+              alt="user-image"
             />
-
-            <div className={"absolute top-[50px] right-[5px] group-hover:block bg-btnDarkHover rounded-[12px] z-[999] text-white min-w-[300px] " + (open ? "block" : "hidden")}>
-                {session ? <div className="px-6 py-4 flex items-center border-b-veryLightBlue border-b ">
-                    <Image height={30} width={30} src={session?.image} className="rounded-full max-w-[30px] max-h-[30px] mr-4" referrerPolicy="no-referrer" alt="user-image" />
-                    <div>
-                        <p className="font-bold">{session?.name}</p>
-                        <p className="font-medium">{session?.email}</p>
-                    </div>
-                </div> : null}
-                <div className="px-6 py-4 ">
-                    <Link href="/appliedCompanies" className="font-medium w-full text-left">Applied Companies</Link>
-
-                </div>
-                <div className="flex item-center ml-auto my-auto px-6 py-4 justify-between">
-                    <p>Theme</p>
-                    <div className="flex shrink-0">
-                        <Image src="/assets/desktop/icon-sun.svg" height={24} width={24} className="md:mr-6 sm:mr-4" alt="icon" />
-                        <Switch />
-                        <Image src="/assets/desktop/icon-moon.svg" height={24} width={24} className="md:ml-6 sm:ml-4" alt="icon" />
-                    </div>
-                </div>
-                <div>
-                    {session ?
-                        <button className="px-6 py-4 font-medium w-full text-left" onClick={signout}>Logout</button> :
-                        <button className="px-6 py-4 font-medium w-full text-left" onClick={login}>Login</button>}
-                </div>
-
+            <div>
+              <p className="font-bold">{session?.name}</p>
+              <p className="font-medium">{session?.email}</p>
             </div>
-
+          </div>
+        ) : null}
+        {session ? (
+          <>
+            <div className="px-6 py-4 ">
+              <Link
+                href="/appliedCompanies"
+                className="font-medium w-full text-left"
+              >
+                <span>
+                  <Icon
+                    path={mdiBriefcaseOutline}
+                    size={1}
+                    className="inline-block mr-3"
+                  />
+                  <span>Applied Companies</span>
+                </span>
+              </Link>
+            </div>
+            <div className="px-6 py-4">
+              <Link href="/profile/edit">
+                <span>
+                  <Icon
+                    path={mdiAccountEdit}
+                    size={1}
+                    className="inline-block mr-3"
+                  />
+                  <span>Update Details</span>
+                </span>
+              </Link>
+            </div>
+            <div className="px-6 py-4">
+              <Link href="/profile/view">
+                {" "}
+                <span>
+                  <Icon
+                    path={mdiAccountDetailsOutline}
+                    size={1}
+                    className="inline-block mr-3"
+                  />
+                  <span>View Profile</span>
+                </span>
+              </Link>
+            </div>
+          </>
+        ) : null}
+        <div className="flex item-center ml-auto my-auto px-6 py-4 justify-between">
+          <p>Theme</p>
+          <div className="flex shrink-0">
+            <Image
+              src="/assets/desktop/icon-sun.svg"
+              height={24}
+              width={24}
+              className="md:mr-6 sm:mr-4"
+              alt="icon"
+            />
+            <Switch />
+            <Image
+              src="/assets/desktop/icon-moon.svg"
+              height={24}
+              width={24}
+              className="md:ml-6 sm:ml-4"
+              alt="icon"
+            />
+          </div>
         </div>
-    )
+        <div>
+          {session ? (
+            <button
+              className="px-6 py-4 font-medium w-full text-left"
+              onClick={signout}
+            >
+              <span>
+                <Icon
+                  path={mdiLockOffOutline}
+                  size={1}
+                  className="inline-block mr-3"
+                />
+                <span>Logout</span>
+              </span>
+            </button>
+          ) : (
+            <button
+              className="px-6 py-4 font-medium w-full text-left"
+              onClick={login}
+            >
+              Login
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
--- components/ProfileWeb.jsx
+++ components/ProfileWeb.jsx
@@ -1,47 +1,121 @@
 import Switch from "@/components/Switch";
-import { mdiAccountCircle } from "@mdi/js";
+import {
+  mdiBriefcaseOutline,
+  mdiAccountEdit,
+  mdiLockOffOutline,
+  mdiAccountDetailsOutline,
+  mdiAccountCircle,
+} from "@mdi/js";
 import Icon from "@mdi/react";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function ProfileWeb({ session, login, signout }) {
-    return (
-        <>
-            <div className="flex item-center h-max ml-auto my-auto z-[998]">
-                <Image src="/assets/desktop/icon-sun.svg" height={24} width={24} className="md:mr-6 sm:mr-4" alt="icon" />
-                <Switch />
-                <Image src="/assets/desktop/icon-moon.svg" height={24} width={24} className="md:ml-6 sm:ml-4" alt="icon" />
+  return (
+    <>
+      <div className="flex item-center h-max ml-auto my-auto z-[998]">
+        <Image
+          src="/assets/desktop/icon-sun.svg"
+          height={24}
+          width={24}
+          className="md:mr-6 sm:mr-4"
+          alt="icon"
+        />
+        <Switch />
+        <Image
+          src="/assets/desktop/icon-moon.svg"
+          height={24}
+          width={24}
+          className="md:ml-6 sm:ml-4"
+          alt="icon"
+        />
+      </div>
+      {session ? (
+        <div className="relative flex items-center ml-6 group">
+          <Icon
+            path={mdiAccountCircle}
+            size="2rem"
+            className="text-white cursor-pointer"
+          />
+          <div className="absolute top-[50px] right-[5px] hidden group-hover:block bg-btnDarkHover rounded-[12px] z-[999] text-white min-w-[300px]">
+            <div className="px-6 py-4 flex items-center border-b-veryLightBlue border-b">
+              <Image
+                height={30}
+                width={30}
+                src={session?.image}
+                className="rounded-full max-w-[30px] max-h-[30px] mr-4"
+                referrerPolicy="no-referrer"
+                alt="user-image"
+              />
+              <div>
+                <p className="font-bold">{session?.name}</p>
+                <p className="font-medium">{session?.email}</p>
+              </div>
             </div>
-            {session ?
-                <div className="relative flex items-center ml-6 group">
-                    <Icon
-                        path={mdiAccountCircle}
-                        size="2rem"
-                        className="text-white cursor-pointer"
-                    />
-                    <div className="absolute top-[50px] right-[5px] hidden group-hover:block bg-btnDarkHover rounded-[12px] z-[999] text-white min-w-[300px]">
-                        <div className="px-6 py-4 flex items-center border-b-veryLightBlue border-b">
-                            <Image height={30} width={30}
-                            src={session?.image} className="rounded-full max-w-[30px] max-h-[30px] mr-4" referrerPolicy="no-referrer" alt="user-image" />
-                            <div>
-                                <p className="font-bold">{session?.name}</p>
-                                <p className="font-medium">{session?.email}</p>
-                            </div>
-
-                        </div>
-                        <Link href="/appliedCompanies" className="px-6 py-4 font-medium w-full text-left">Applied Companies</Link>
-                        <div>
-                            <button className="px-6 py-4 font-medium w-full text-left" onClick={signout}>Logout</button>
-                        </div>
-                    </div>
-                </div> :
-                <div
-                    className="z-[999] rounded-[6px] text-lightBlue relative ml-10 py-4 px-10 font-bold my-auto cursor-pointer flex items-center before:bg-gradient-to-r from-white to-darkGrey before:h-[100%] bg-white hover:bg-veryLightBlue hover:text-white before:w-[140%] before:absolute overflow-hidden bg-veryDarkBlue after:rounded-[6px] after:content-[''] after:absolute after:inset-[5px] after:bg-lightBlue before:inset-[0] before:animate-btnAnimated before:rounded-[6px] before:hidden after:hidden hover:before:block hover:after:block flex justify-center"
-                    onClick={login}
-                >
-                    <p className="z-[999]">Login</p>
-                </div>
-            }
-        </>
-    )
+            <div className="px-6 py-4 ">
+              <Link
+                href="/appliedCompanies"
+                className="font-medium w-full text-left"
+              >
+                <span>
+                  <Icon
+                    path={mdiBriefcaseOutline}
+                    size={1}
+                    className="inline-block mr-3"
+                  />
+                  <span>Applied Companies</span>
+                </span>
+              </Link>
+            </div>
+            <div className="px-6 py-4 font-medium w-full text-left">
+              <Link href="/profile/edit">
+                <span>
+                  <Icon
+                    path={mdiAccountEdit}
+                    size={1}
+                    className="inline-block mr-3"
+                  />
+                  <span>Update Details</span>
+                </span>
+              </Link>
+            </div>
+            <div className="px-6 py-4 font-medium w-full text-left">
+              <Link href="/profile/view">
+                <span>
+                  <Icon
+                    path={mdiAccountDetailsOutline}
+                    size={1}
+                    className="inline-block mr-3"
+                  />
+                  <span>View Profile</span>
+                </span>
+              </Link>
+            </div>
+            <div>
+              <button
+                className="px-6 py-4 font-medium w-full text-left"
+                onClick={signout}
+              >
+                <span>
+                  <Icon
+                    path={mdiLockOffOutline}
+                    size={1}
+                    className="inline-block mr-3"
+                  />
+                  <span>Logout</span>
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div
+          className="z-[999] rounded-[6px] text-lightBlue relative ml-10 py-4 px-10 font-bold my-auto cursor-pointer items-center before:bg-gradient-to-r from-white to-darkGrey before:h-[100%] bg-white hover:bg-veryLightBlue hover:text-white before:w-[140%] before:absolute overflow-hidden bg-veryDarkBlue after:rounded-[6px] after:content-[''] after:absolute after:inset-[5px] after:bg-lightBlue before:inset-[0] before:animate-btnAnimated before:rounded-[6px] before:hidden after:hidden hover:before:block hover:after:block flex justify-center"
+          onClick={login}
+        >
+          <p className="z-[999]">Login</p>
+        </div>
+      )}
+    </>
+  );
 }
